e71ccc23037009dd2a06c2a8c0fb79c4bfeb0264
RootShell-coder <RootShelling@gmail.com>
2020-10-18 18:50:46 +0300

    Led PWM warm/cold

--- src/main.cpp
+++ src/main.cpp
@@ -3,25 +3,30 @@
 #include "sensorNode.h"
 #include "remoteNode.h"
 #include "ambilightNode.h"
+#include "ledNode.h"
 
 #define BRAND     "HSL"
 #define FM_NAME   "The Smart Lamp"
-#define FM_VER    "0.0.2"
+#define FM_VER    "0.0.3"
 
 pingNode PingNode("network", "connection", "ping");
 sensorNode SensorNode("sensor", "environment", "BME280");
 remoteNode RemoteNode("remote", "control", "IR");
 ambilightNode AmbilihgtNode("ambilight", "Led", "WS2812B");
+ledNode LedNode("light", "Led", "PWM");
 
 void setup() {
   Serial.begin(115200);
   Serial << endl << endl;
   Homie_setBrand(BRAND);
   Homie_setFirmware(FM_NAME, FM_VER);
+
   PingNode.pingSetup();
   SensorNode.sensorSetup();
   RemoteNode.remoteSetup();
   AmbilihgtNode.ambilightSetup();
+  LedNode.ledSetup();
+
   Homie.setup();
 }
 
